1cc9a60a310953d75eaa8be650933515fd3878cd
LYH <cpi1234@snu.ac.kr>
2020-06-07 21:01:35 +0900

    뇌피셜로 코딩하던 중. multiGPU를 활용하려면 loss 구하는 부분을까지 forward에서 처리해야하서 forward에 stage 개념을 넣어서 loss를 다르게 구할 수 있도록 하면 어떨까

--- hparams.py
+++ hparams.py
@@ -7,7 +7,7 @@ seed=1234
 n_gpus=2
 output_directory = 'training_log'
 log_directory = 'transformer-tts-phone'
-data_path = '/media/disk1/lyh/LJSpeech-1.1/preprocessed'
+data_path = '../Dataset/lyh/LJSpeech-1.1/preprocessed'
 teacher_path = '/media/disk1/lyh/fastspeech_phone'
 
 training_files='filelists/ljs_audio_text_train_filelist.txt'
@@ -36,9 +36,8 @@ hidden_dim=256
 dprenet_dim=256
 postnet_dim=256
 ff_dim=1024
-n_heads=4
+n_heads=2
 n_layers=6
-n_postnet_layers=5
 
 
 ################################
--- modules/model.py
+++ modules/model.py
@@ -6,259 +6,152 @@ from .transformer import *
 from utils.utils import get_mask_from_lengths
 
 
-class CBAD(nn.Module):
-    def __init__(self,
-                 in_dim,
-                 out_dim,
-                 kernel_size,
-                 stride,
-                 padding,
-                 bias,
-                 activation,
-                 dropout):
-        super(CBAD, self).__init__()
-        self.conv = Conv1d(in_dim,
-                           out_dim,
-                           kernel_size=kernel_size,
-                           stride=stride,
-                           padding=padding,
-                           bias=bias,
-                           w_init_gain=activation)
+class FFT(nn.Module):
+    def __init__(self, hidden_dim, n_heads, ff_dim, n_layers):
+        super(FFT, self).__init__()
+        self.FFT_layers = nn.ModuleList([TransformerEncoderLayer(d_model=hidden_dim,
+                                                                 nhead=n_heads,
+                                                                 dim_feedforward=ff_dim)
+                                      for _ in range(n_layers)])
 
-        self.bn = nn.BatchNorm1d(out_dim)
+    def forward(self, x, lengths):
+        alignments = []
+        mask = get_mask_from_lengths(lengths)
+        for layer in self.FFT_layers:
+            x, align = layer(x, src_key_padding_mask=mask)
+            alignments.append(align.unsqueeze(1))
+        alignments = torch.cat(alignments, 1)
 
-        if activation == 'relu':
-            self.activation = nn.ReLU()
-        elif activation == 'tanh':
-            self.activation = nn.Tanh()
-
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, x):
-        x = self.conv(x)
-        x = self.bn(x)
-        x = self.activation(x)
-        out = self.dropout(x)
-
-        return out
+        return x, alignments
 
 
-class Prenet_D(nn.Module):
+class MixDensityNetwork(nn.Module):
     def __init__(self, hp):
-        super(Prenet_D, self).__init__()
-        self.linear1 = Linear(hp.n_mel_channels,
-                              hp.dprenet_dim,
-                              w_init_gain='relu')
-        self.linear2 = Linear(hp.dprenet_dim, hp.dprenet_dim, w_init_gain='relu')
-        self.linear3 = Linear(hp.dprenet_dim, hp.hidden_dim)
+        super(MixDensityNetwork, self).__init__()
+        self.linear = nn.ModuleList([Linear(hp.hidden_dim, 256),
+                                     nn.LayerNorm(256),
+                                     nn.ReLU(),
+                                     nn.Dropout(0.1),
+                                     Linear(256, 2*hp.n_mel_channels),
+                                     nn.LayerNorm(2*hp.n_mel_channels),
+                                     nn.ReLU()])
 
-    def forward(self, x):
-        # Set training==True following tacotron2
-        x = F.dropout(F.relu(self.linear1(x)), p=0.5, training=True)
-        x = F.dropout(F.relu(self.linear2(x)), p=0.5, training=True)
-        x = self.linear3(x)
-        return x
+    def forward(self, hidden_states):
+        mu_sigma = self.linear(hidden_states)
+        return mu_sigma
 
 
-class PostNet(nn.Module):
-    def __init__(self, hp):
-        super(PostNet, self).__init__()
-        conv_list = [CBAD(in_dim=hp.n_mel_channels,
-                          out_dim=hp.postnet_dim,
-                          kernel_size=5,
-                          stride=1,
-                          padding=2,
-                          bias=False,
-                          activation='tanh',
-                          dropout=0.5)]
-        
-        for _ in range(hp.n_postnet_layers-2):
-            conv_list.append(CBAD(in_dim=hp.postnet_dim,
-                                  out_dim=hp.postnet_dim,
-                                  kernel_size=5,
-                                  stride=1,
-                                  padding=2,
-                                  bias=False,
-                                  activation='tanh',
-                                  dropout=0.5))
-            
-        conv_list.append(nn.Sequential(nn.Conv1d(hp.postnet_dim,
-                                                 hp.n_mel_channels,
-                                                 kernel_size=5,
-                                                 padding=2,
-                                                 bias=False),
-                                       nn.BatchNorm1d(hp.n_mel_channels),
-                                       nn.Dropout(0.5)))
-        
-        self.conv=nn.ModuleList(conv_list)
+class DurationPredictor(nn.Module):
+    def __init__(self, hidden_dim, n_heads, ff_dim, n_layers):
+        super(DurationPredictor, self).__init__()
+        self.FFT = FFT(hidden_dim, n_heads, ff_dim, n_layers)
+        self.linear = Linear(hidden_dim,hidden_dim)
 
-    def forward(self, x):
-        for conv in self.conv:
-            x = conv(x)
-        return x
-
+    def forward(self, encoder_input, text_lengths):
+        durations = self.linear(self.FFT(encoder_input, text_lengths))
+        return durations
+    
 
 class Model(nn.Module):
     def __init__(self, hp):
         super(Model, self).__init__()
         self.hp = hp
         self.Embedding = nn.Embedding(hp.n_symbols, hp.symbols_embedding_dim)
-        self.Prenet_D = Prenet_D(hp)
-        
-        self.alpha1 = nn.Parameter(torch.ones(1))
-        self.alpha2 = nn.Parameter(torch.ones(1))
         self.register_buffer('pe', PositionalEncoding(hp.hidden_dim).pe)
         self.dropout = nn.Dropout(0.1)
         
-        self.Encoder = nn.ModuleList([TransformerEncoderLayer(d_model=hp.hidden_dim,
-                                                              nhead=hp.n_heads,
-                                                              dim_feedforward=hp.ff_dim)
-                                      for _ in range(hp.n_layers)])
-
-        self.Decoder = nn.ModuleList([TransformerDecoderLayer(d_model=hp.hidden_dim,
-                                                              nhead=hp.n_heads,
-                                                              dim_feedforward=hp.ff_dim)
-                                      for _ in range(hp.n_layers)])
-
+        self.FFT_lower = FFT(hp.hidden_dim, hp.n_heads, hp.ff_dim, hp.n_layers)
+        self.FFT_upper = FFT(hp.hidden_dim, hp.n_heads, hp.ff_dim, hp.n_layers)
+        self.MDN = MixDensityNetwork(hp)
+        self.DurationPredictor = DurationPredictor(hp.hidden_dim, hp.n_heads, hp.ff_dim, hp.n_layers)
         self.Projection = Linear(hp.hidden_dim, hp.n_mel_channels)
-        self.Postnet = PostNet(hp)
-        self.Stop = nn.Linear(hp.n_mel_channels, 1)
         
         
-    def outputs(self, text, melspec, text_lengths, mel_lengths):
+    def get_hidden_states(self, text, text_lengths):
         ### Size ###
         B, L, T = text.size(0), text.size(1), melspec.size(2)
         
         ### Prepare Encoder Input ###
         encoder_input = self.Embedding(text).transpose(0,1)
-        encoder_input += self.alpha1*(self.pe[:L].unsqueeze(1))
+        encoder_input += self.pe[:L].unsqueeze(1)
         encoder_input = self.dropout(encoder_input)
-
-        ### Prepare Decoder Input ###
-        mel_input = F.pad(melspec, (1,-1)).transpose(1,2)
-        decoder_input = self.Prenet_D(mel_input).transpose(0,1)
-        decoder_input += self.alpha2*(self.pe[:T].unsqueeze(1))
-        decoder_input = self.dropout(decoder_input)
-
-        ### Prepare Masks ###
-        text_mask = get_mask_from_lengths(text_lengths)
-        mel_mask = get_mask_from_lengths(mel_lengths)
-        diag_mask = torch.triu(melspec.new_ones(T,T)).transpose(0, 1)
-        diag_mask[diag_mask == 0] = -float('inf')
-        diag_mask[diag_mask == 1] = 0
-
-        ### Transformer Encoder ###
-        memory = encoder_input
-        enc_alignments = []
-        for layer in self.Encoder:
-            memory, enc_align = layer(memory, src_key_padding_mask=text_mask)
-            enc_alignments.append(enc_align.unsqueeze(1))
-        enc_alignments = torch.cat(enc_alignments, 1)
-
-        ### Transformer Decoder ###
-        tgt = decoder_input
-        dec_alignments, enc_dec_alignments = [], []
-        for layer in self.Decoder:
-            tgt, dec_align, enc_dec_align = layer(tgt,
-                                                  memory,
-                                                  tgt_mask=diag_mask,
-                                                  tgt_key_padding_mask=mel_mask,
-                                                  memory_key_padding_mask=text_mask)
-            dec_alignments.append(dec_align.unsqueeze(1))
-            enc_dec_alignments.append(enc_dec_align.unsqueeze(1))
-        dec_alignments = torch.cat(dec_alignments, 1)
-        enc_dec_alignments = torch.cat(enc_dec_alignments, 1)
-
-        ### Projection + PostNet ###
-        mel_out = self.Projection(tgt.transpose(0, 1)).transpose(1, 2)
-        mel_out_post = self.Postnet(mel_out) + mel_out
-
-        gate_out = self.Stop(mel_out.transpose(1, 2)).squeeze(-1)
+        hidden_states, enc_alignments = FFT_lower(encoder_input, text_lengths)
         
-        return mel_out, mel_out_post, enc_alignments, dec_alignments, enc_dec_alignments, gate_out
+        return hidden_states, enc_alignments
     
+    def get_mu_sigma(self, hidden_states):
+        mu_sigma = self.MDN(hidden_states)
+        return mu_sigma
     
-    def forward(self, text, melspec, gate, text_lengths, mel_lengths, criterion):
-        ### Size ###
-        text = text[:,:text_lengths.max().item()]
-        melspec = melspec[:,:,:mel_lengths.max().item()]
-        gate = gate[:, :mel_lengths.max().item()]
-        outputs = self.outputs(text, melspec, text_lengths, mel_lengths)
+    def get_duration(self, encoder_input, text_lengths):
+        durations = self.DurationPredictor(encoder_input, text_lengths)
+        return durations
+    
+    def get_melspec(self, hidden_states, text_lengths)
+    
+    def forward(self, text, melspec, gate, text_lengths, mel_lengths, criterion, step):
+        hidden_states = get_hidden_states(self, text, text_lengths)
         
-        mel_out, mel_out_post = outputs[0], outputs[1]
-        enc_dec_alignments = outputs[4]
-        gate_out=outputs[5]
+        if step==0:
+            mu_sigma=get_mu_sigma(self, hidden_states)
+            mdn_loss = MDN_LOSS(mu_sigma, melspec)
+            return mdn_loss
         
-        mel_loss, bce_loss, guide_loss = criterion((mel_out, mel_out_post, gate_out),
-                                                   (melspec, gate),
-                                                   (enc_dec_alignments, text_lengths, mel_lengths))
+        elif step==1:
+            ## return FFT_LOSS
+        elif step==2:
+            ## return MDN_LOSS + FFT_LOSS
+        elif step==3:
+            ## return duration_LOSS
         
-        return mel_loss, bce_loss, guide_loss
+    def inference(self, text, alpha=1.0):
+        ### Prepare Inference ###
+        text_lengths = torch.tensor([1, text.size(1)])
         
-        
-    def inference(self, text, max_len=1024):
-        ### Size & Length ###
-        (B, L), T = text.size(), max_len
-
         ### Prepare Inputs ###
-        encoder_input = self.Embedding(text).transpose(0,1).contiguous()
-        encoder_input += self.alpha1*(self.pe[:L].unsqueeze(1))
+        encoder_input = self.Embedding(text).transpose(0,1)
+        encoder_input += self.pe[:text.size(1)].unsqueeze(1)
+        
+        ### Speech Synthesis ###
+        text_mask = text.new_zeros(1,text.size(1)).to(torch.bool)
+        hidden_states = self.FFT_lower(encoder_input, text_lengths)
+            
+        ### Duration Predictor ###
+        durations = self.DurationPredictor(hidden_states.transpose(0,1))
+        hidden_states_expanded = self.LR(hidden_states, durations, alpha, inference=True)
+        hidden_states_expanded += self.pe[:hidden_states_expanded.size(0)].unsqueeze(1)
+        mel_lengths = hidden_states_expanded.size(0)
+        
+        hidden_states_expanded = self.FFT_upper(hidden_states_expanded, mel_lengths)
+        mel_out = self.Projection(hidden_states_expanded.transpose(0,1)).transpose(1,2)
+        
+        return mel_out, durations
 
-        ### Prepare Masks ###
-        text_mask  = text.new_zeros(1, L).to(torch.bool)
-        mel_mask = text.new_zeros(1, T).to(torch.bool)
-        diag_mask = torch.triu(text.new_ones(T, T)).transpose(0, 1).contiguous()
-        diag_mask[diag_mask == 0] = -1e9
-        diag_mask[diag_mask == 1] = 0
+    
+    def align2duration(self, alignments, mel_mask):
+        ids = alignments.new_tensor( torch.arange(alignments.size(2)) )
+        max_ids = torch.max(alignments, dim=2)[1].unsqueeze(-1)
+        
+        one_hot = 1.0*(ids==max_ids)
+        one_hot.masked_fill_(mel_mask.unsqueeze(2), 0)
+        
+        durations = torch.sum(one_hot, dim=1)
 
-        ### Transformer Encoder ###
-        memory = encoder_input
-        enc_alignments = []
-        for layer in self.Encoder:
-            memory, enc_align = layer(memory, src_key_padding_mask=text_mask)
-            enc_alignments.append(enc_align)
-        enc_alignments = torch.cat(enc_alignments, dim=0)
-
-        ### Transformer Decoder ###
-        mel_input = text.new_zeros(1,
-                                   self.hp.n_mel_channels,
-                                   max_len).to(torch.float32)
-        dec_alignments = text.new_zeros(self.hp.n_layers,
-                                        self.hp.n_heads,
-                                        max_len,
-                                        max_len).to(torch.float32)
-        enc_dec_alignments = text.new_zeros(self.hp.n_layers,
-                                            self.hp.n_heads,
-                                            max_len,
-                                            text.size(1)).to(torch.float32)
-
-        ### Generation ###
-        stop=[]
-        for i in range(max_len):
-            tgt = self.Prenet_D(mel_input.transpose(1,2).contiguous()).transpose(0,1).contiguous()
-            tgt += self.alpha2*(self.pe[:T].unsqueeze(1))
-
-            for j, layer in enumerate(self.Decoder):
-                tgt, dec_align, enc_dec_align = layer(tgt,
-                                                      memory,
-                                                      tgt_mask=diag_mask,
-                                                      tgt_key_padding_mask=mel_mask,
-                                                      memory_key_padding_mask=text_mask)
-                dec_alignments[j, :, i] = dec_align[0, :, i]
-                enc_dec_alignments[j, :, i] = enc_dec_align[0, :, i]
-
-            mel_out = self.Projection(tgt.transpose(0,1).contiguous())
-            stop.append(torch.sigmoid(self.Stop(mel_out[:,i]))[0,0].item())
-
-            if i < max_len - 1:
-                mel_input[0, :, i+1] = mel_out[0, i]
-                
-            if stop[-1]>0.5:
-                break
-
-        mel_out_post = self.Postnet(mel_out.transpose(1, 2).contiguous())
-        mel_out_post = mel_out_post.transpose(1, 2).contiguous() + mel_out
-        mel_out_post = mel_out_post.transpose(1, 2).contiguous()
-
-        return mel_out_post, enc_alignments, dec_alignments, enc_dec_alignments, stop
-
+        return durations
+    
+    
+    def LR(self, hidden_states, durations, alpha=1.0, inference=False):
+        L, B, D = hidden_states.size()
+        durations = torch.round(durations*alpha).to(torch.long)
+        if inference:
+            durations[durations<=0]=1
+        T=int(torch.sum(durations, dim=-1).max().item())
+        expanded = hidden_states.new_zeros(T, B, D)
+        
+        for i, d in enumerate(durations):
+            mel_len = torch.sum(d).item()
+            expanded[:mel_len, i] = torch.repeat_interleave(hidden_states[:, i],
+                                                            d,
+                                                            dim=0)
+            
+        return expanded
