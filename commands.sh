7c629fee1b1a72fad25ff34d0e867428c8b327e9
LEE YOON HYUNG <40514540+LEEYOONHYUNG@users.noreply.github.com>
2020-07-09 22:13:31 +0900

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# python train.py --stage=0 &&
-# python extract_alignments.py &&
+python train.py --stage=0 &&
+python extract_alignments.py &&
 python train.py --stage=1 &&
 python train.py --stage=2 &&
 python extract_alignments.py &&
