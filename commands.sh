f8bd9a6797f987e4233b49b3b05c561336335932
LYH <cpi1234@snu.ac.kr>
2020-07-09 22:03:07 +0900

    update

---
+++ commands.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# python train.py --stage=0 &&
+# python extract_alignments.py &&
+python train.py --stage=1 &&
+python train.py --stage=2 &&
+python extract_alignments.py &&
+python train.py --stage=3
